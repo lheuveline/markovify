441c511debbe4ca0455e92af761c1caeb02c2e8d
lheuveline <l.heuveline@louisheuveline.com>
2023-05-27 19:39:32 +0200

    adding filtering mechasnism to target specific lines

--- markovify/parallel.py
+++ markovify/parallel.py
@@ -12,23 +12,37 @@ import json
 import markovify
 import os
 from tqdm import tqdm
-
+import re
     
+class NoWordException(Exception): pass
+
 class TextFromComboList(markovify.Text):
 
     def sentence_split(self, text):
         # return List[List], list of chars in list of words, words[chars]
         output = []
         for line in text.split("\n"):
-            splitted = line.split(":")[:2]
-            if len(splitted) > 1:
-                name, word = splitted
-            if filter_username(name, pattern_list=FILTER_PATTERN):
-                output.append(" ".join(list(word)))
+            name, word = self.process_line(line)
+            if name and word:
+                if filter_username(name, pattern_list=FILTER_PATTERN):
+                    output.append(word)
         if len(output) > 0:
             return output
         else:
-            raise Exception("No word found in list using pattern :", FILTER_PATTERN)
+            raise NoWordException("No word found in list using pattern :", FILTER_PATTERN)
+        
+    def process_line(self, line):
+
+        splitted = line.split(":")[:2]
+        if len(splitted) > 1:
+            name, word = splitted
+            name = "".join(x for x in name if x.isascii())
+            if len(name) > 0 and len(word) > 0:
+                return name, word
+            else:
+                return None, None
+        else:
+            return None, None
 
 def get_filelist(source):
     matches = []
@@ -45,9 +59,15 @@ def filter_username(text, pattern_list=None):
 
     if pattern_list:
         pattern_list = pattern_list.split(",")
-        return any(pattern in text for pattern in pattern_list)
+        matches = []
+        for pattern in pattern_list:
+            match = re.findall(pattern, text)
+            if len(match) > 0:
+                matches.append(match)
+        if len(matches) > 0:
+            return text
     else:
-        return True
+        return text
 
 # def clean_text(text):
 
@@ -76,7 +96,10 @@ def format_batch(filelist, batch_size=8):
     return filelist, texts, batch_filelist
 
 def train_model(text):
-    model = TextFromComboList(text, retain_original=False)
+    try:
+        model = TextFromComboList(text, retain_original=False)
+    except NoWordException as e:
+        return None
     return model
 
 def main_loop(args):
@@ -117,11 +140,17 @@ def main_loop(args):
     with Pool(n_cpu - 1) as pool:
         with tqdm(total=n_batches) as pbar:
             while len(filelist) > 0:
-                filelist, texts, _ = format_batch(
+                filelist, texts, batch = format_batch(
                     filelist,
                     args.batch_size
                     )
                 intermediate_models = pool.map(train_model, texts)
+                
+                # Clean models and display corresponding files
+                for i, model in enumerate(intermediate_models):
+                    if not model:
+                        intermediate_models.pop(i)
+                        print("ERROR:Error training model on :", batch[i])
 
                 # Combine model with previous batch models
                 if combined_model:
